0bb6a740e6af26e868b26db1b35ea466967c3a03
Marco Anghinetti <marcoanghinetti@gmail.com>
2023-09-25 13:04:22 +0200

    Added invite link to play with friends and custom color theme

--- index.js
+++ index.js
@@ -16,21 +16,37 @@ app.get('/', (req, res) => {
 
 //#region socket.io
 let waitingSockets = [];
+let waitingFriend = {};
 io.on('connection', (socket) => {
 	socket.on("newgame", (nick) => {
+		let turn = Math.round(Math.random());
 		let s2 = waitingSockets.shift();
 		if(s2 != undefined && s2.socket.id != socket.id) {
 			let id = socket.id+s2.socket.id;
 			socket.join(id);
 			s2.socket.join(id);
-			socket.emit("start", {yourTurn: 1, nick: s2.nick, id: id});
-			s2.socket.emit("start", {yourTurn: 0, nick: nick, id: id});
+			socket.emit("start", {yourTurn: turn, nick: s2.nick, id: id});
+			s2.socket.emit("start", {yourTurn: 1-turn, nick: nick, id: id});
 		} else if(s2 == undefined){
 			waitingSockets.push({socket: socket, nick: nick});
 		} else {
 			waitingSockets.unshift(s2);
 		}
 	});
+	socket.on("invite", (nick) => {
+		waitingFriend[socket.id] = {socket: socket, nick: nick};
+	});
+	socket.on("invited", (data) => {
+		let s2 = waitingFriend[data.id];
+		if(s2 == undefined) return;
+		let turn = Math.round(Math.random());
+		let id = socket.id+s2.socket.id;
+		socket.join(id);
+		s2.socket.join(id);
+		socket.emit("start", {yourTurn: turn, nick: s2.nick, id: id});
+		s2.socket.emit("start", {yourTurn: 1-turn, nick: data.nick, id: id});
+		delete waitingFriend[data.id];
+	})
 	socket.on("place", (data) => {
 		if(socket.rooms.has(data.id)) {
 			io.to(data.id).emit("place", data);
@@ -48,12 +64,15 @@ io.on('connection', (socket) => {
 				io.to(r).emit("won");
 			}
 		}
+		if(waitingFriend[socket.id] != undefined) {
+			delete waitingFriend[socket.id];
+		}
 	});
 });
 
 //#endregion
 
-/**/
+/**
 let port = process.env.PORT || 3000;
 server.listen(port, "0.0.0.0", () => {
 	console.log('Server running on port:'+port);
